9db96c0cc8f882cac68b9827becf5ec0f8a6e034
JECunanan <jecunanan.learning@gmail.com>
2024-02-20 15:19:03 +0800

    Updated comments

--- FirstLaboratory/FirstLaboratory.java
+++ FirstLaboratory/FirstLaboratory.java
@@ -1,3 +1,12 @@
+/*
+AMAOEd MIS6232 Computer Programming 2 (Java)
+
+Author: J. E. Cunanan
+Student No: 2022-0072254
+
+Laboratory Exercise 001
+*/
+
 package FirstLaboratory;
 
 public class FirstLaboratory {
