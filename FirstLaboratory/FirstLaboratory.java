bed971bc4a5f1de42fa4e73c9680c5a25a3b85d5
JECunanan <jecunanan.learning@gmail.com>
2024-02-20 15:13:10 +0800

    1st Lab, arranged files

---
+++ FirstLaboratory/FirstLaboratory.java
@@ -0,0 +1,7 @@
+package FirstLaboratory;
+
+public class FirstLaboratory {
+    public static void main(String[] args) {
+        System.out.println("This is my first laboratory exercise.");
+    }
+}
