f6643e4cbe6ef76ef702b64cd303dd2e6d7e070f
JECunanan <jecunanan.learning@gmail.com>
2024-02-22 10:58:39 +0800

    Test Stem 2

--- Lab002/LaboratoryExercise2.java
+++ Lab002/LaboratoryExercise2.java
@@ -2,10 +2,6 @@ package Lab002;
 
 public class LaboratoryExercise2 {
     public static void main(String[] args) {
-        // Create non-initialized int data type variable;
-        int x;
-
-        // Print the variable (will require variable to be initialized to function)
-        System.out.println(x);
+        System.out.println("\"Hello\" world!");
     }
 }
