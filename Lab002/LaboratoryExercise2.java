7887426190db629e4ce3d568340b1f6b9db90fae
JECunanan <jecunanan.learning@gmail.com>
2024-02-22 11:06:24 +0800

    Test Stem 4

--- Lab002/LaboratoryExercise2.java
+++ Lab002/LaboratoryExercise2.java
@@ -4,6 +4,6 @@ public class LaboratoryExercise2 {
     public static void main(String[] args) {
         // Test Stem deliberate error (var a is not an int data type)
         int a = "Hello world!";
-        System.out.println("a");
+        System.out.println(A);
     }
 }
