2193bd1622fc01dba94275e359d73cd6e06e1470
JECunanan <jecunanan.learning@gmail.com>
2024-02-22 11:20:28 +0800

    Lab002 #6 answered

---
+++ Lab002/Lab2_6.java
@@ -0,0 +1,8 @@
+package Lab002;
+
+public class Lab2_6 {
+    public static void main(String[] args) {
+        System.out.println("\"I am writing a program that will display \"text\" with escape characters. At the beginning of this sentence, it has a tab escape character.");
+        System.out.println("\n\nThere are two new lines before this sentence and you also have to include 'single quote' and \\back slash\\ characters in this sentence.\"");
+    }
+}
---
+++ Lab002/Lab2_7.java
@@ -0,0 +1,2 @@
+package Lab002;public class Lab2_7 {
+}
