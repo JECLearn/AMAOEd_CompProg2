9dd3cefa4cf5cd305b763426e4ea4786dd41e05e
JECunanan <jecunanan.learning@gmail.com>
2024-02-22 11:41:24 +0800

    Lab002 #7 answered

--- Lab002/Lab2_7.java
+++ Lab002/Lab2_7.java
@@ -1,2 +1,29 @@
-package Lab002;public class Lab2_7 {
+package Lab002;
+
+public class Lab2_7 {
+    public static void main(String[] args) {
+        // variable initialization
+        float Gross_Sales = 0.0F;
+        int Age = 1;
+        boolean Final = false;
+        String Location = "0x11";
+        String Old_File = "067";
+        char Default = 'a';
+        String Name = "noname";
+
+        // convert hex to decimal for String Location
+        int decimalLocation = Integer.parseInt(Location.substring(2),16);
+
+        // convert octal to decimal for String Old_File
+        int octalOld_File = Integer.parseInt(Old_File, 8);
+
+        // print variables
+        System.out.println("Gross sales = " + Gross_Sales);
+        System.out.println("Age = " + Age);
+        System.out.println("Final = " + Final);
+        System.out.println("Location = " + decimalLocation);
+        System.out.println("Old File = " + octalOld_File);
+        System.out.println("Default = " + Default);
+        System.out.println("Name = " + Name);
+    }
 }
