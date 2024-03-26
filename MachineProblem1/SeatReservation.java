3de935c2d6218ff969c676d5fcaa8d563ae37a77
JECunanan <jecunanan.learning@gmail.com>
2024-03-26 10:03:19 +0800

    class created

---
+++ MachineProblem1/SeatReservation.java
@@ -0,0 +1,15 @@
+/*
+AMAOEd MIS6232 Computer Programming 2 (Java)
+
+Author: J. E. Cunanan
+Student No: 2022-0072254
+
+Machine Problem 1
+*/
+package MachineProblem1;
+
+public class SeatReservation {
+    public static void main(String[] args) {
+
+    }
+}
