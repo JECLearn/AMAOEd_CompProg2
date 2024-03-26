9d6d60957d21587101a9d1102e643b1adad264e1
JECunanan <jecunanan.learning@gmail.com>
2024-03-26 10:37:09 +0800

    seat grid function

--- MachineProblem1/SeatReservation.java
+++ MachineProblem1/SeatReservation.java
@@ -7,13 +7,27 @@ Student No: 2022-0072254
 Machine Problem 1
 */
 package MachineProblem1;
+import java.util.Scanner;
 
 public class SeatReservation {
+    // seat grid function
+    static final int rows = 5;
+    static final int columns = 7;
+    public static void seatGrid(int[][] seats) {
+        System.out.println();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (seats[i][j] == 0) {
+                    System.out.printf("%5s", "0");
+                } else {
+                    System.out.printf("%5d", seats[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-
-        // seat grid function
-        int rows = 5;
-        int columns = 7;
-
+        int[][] seats = new int[rows][columns];
+        seatGrid(seats);
     }
 }
