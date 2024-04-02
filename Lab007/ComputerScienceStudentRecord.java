263b0a839ebe7a66dd8312a9e21b83518255090a
JECunanan <jecunanan.learning@gmail.com>
2024-04-02 11:37:54 +0800

    organized files

---
+++ Lab007/ComputerScienceStudentRecord.java
@@ -0,0 +1,22 @@
+/*
+AMAOEd MIS6232 Computer Programming 2 (Java)
+
+Author: J. E. Cunanan
+Student No: 2022-0072254
+
+Laboratory Exercise 007
+*/
+package Lab007;
+
+public class ComputerScienceStudentRecord extends StudentRecord {
+
+    // Question 5
+    private int computerProgrammingGrade;
+
+    // Question 3
+    public ComputerScienceStudentRecord(String name, double mathGrade, double englishGrade, double scienceGrade) {
+        // Question 4
+        super(name, mathGrade, englishGrade, scienceGrade);
+
+    }
+}
---
+++ Lab007/StudentRecord.java
@@ -0,0 +1,54 @@
+package Lab007;
+
+// StudentRecord
+public class StudentRecord {
+
+    //these are the attributes
+    private String name;
+    private double mathGrade;
+    private double englishGrade;
+    private double scienceGrade;
+
+    // Constructor
+    public StudentRecord(String name, double mathGrade, double englishGrade, double scienceGrade) {
+    }
+
+    //these are the mutators and accessors
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMathGrade() {
+        return mathGrade;
+    }
+
+    public void setMathGrade(double mathGrade) {
+        this.mathGrade = mathGrade;
+    }
+
+    public double getEnglishGrade() {
+        return englishGrade;
+    }
+
+    public void setEnglishGrade(double englishGrade) {
+        this.englishGrade = englishGrade;
+    }
+
+    public double getScienceGrade() {
+        return scienceGrade;
+    }
+
+    public void setScienceGrade(double scienceGrade) {
+        this.scienceGrade = scienceGrade;
+    }
+
+    //custom method
+    public double computeAverageGrade(){
+        return (this.mathGrade + this.englishGrade + this.scienceGrade)/3;
+    }
+
+}
