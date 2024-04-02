857a2bbc29df187ad958dcb911148fc64755140c
JECunanan <jecunanan.learning@gmail.com>
2024-04-02 10:43:44 +0800

    Lab007 files init

---
+++ Lab007/Lab8Main.java
@@ -0,0 +1,15 @@
+/*
+AMAOEd MIS6232 Computer Programming 2 (Java)
+
+Author: J. E. Cunanan
+Student No: 2022-0072254
+
+Laboratory Exercise 007
+*/
+package Lab007;
+
+public class Lab8Main {
+    public static void main(String[] args) {
+
+    }
+}
