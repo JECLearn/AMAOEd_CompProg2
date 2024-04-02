c95a23c4ac546d9aac6fcf77d602abafc4da021f
JECunanan <jecunanan.learning@gmail.com>
2024-04-02 12:41:44 +0800

    Lab008 answered

--- Lab008/Lab008.java
+++ Lab008/Lab008.java
@@ -19,6 +19,23 @@ public class Lab008 {
 
         // Print hashCode
         System.out.println("Hash: " + hashCode);
+
+        // Question 4
+        NewClass person1 = new NewClass("J. E. Cunanan", "69 Ligaya Street");
+        System.out.println(person1.getName() + ", " + person1.getAddress());
+
+        // Question 7
+        String spaces = " This has a space in the beginning and end. ";
+        String trimmedSpaces = spaces.trim();
+        System.out.println("Original: " + spaces);
+        System.out.println("Trimmed: " + trimmedSpaces);
+
+        // Question 8
+        StringBuffer sb = new StringBuffer("hello");
+
+        // Question 9
+        sb.setLength(2);
+        System.out.println("StringBuffer: " + sb.toString());
     }
 
 }
