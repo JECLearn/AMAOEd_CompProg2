e070a3537b38650508ff79555e56e2b97994063d
JECunanan <jecunanan.learning@gmail.com>
2024-04-03 11:43:47 +0800

    files init

---
+++ MachineProblem3/SavingsAccount.java
@@ -0,0 +1,16 @@
+package MachineProblem3;
+
+/*
+AMAOEd MIS6232 Computer Programming 2 (Java)
+
+Author: J. E. Cunanan
+Student No: 2022-0072254
+
+Machine Problem 3 (Course Project)
+*/
+
+public class SavingsAccount {
+
+
+
+}
