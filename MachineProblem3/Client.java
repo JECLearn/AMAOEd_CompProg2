b921870f7a9d2f32f16d0068476465d47b093943
JECLearn <94827754+JECLearn@users.noreply.github.com>
2024-04-03 15:38:19 +0800

    Main Menu init

--- MachineProblem3/Client.java
+++ MachineProblem3/Client.java
@@ -13,7 +13,18 @@ public class Client {
 
     public static void main(String[] args) {
 
-
+        // Main Menu Display
+        System.out.println("                 Welcome to AMA Bank                 ");
+        System.out.println("--------------------- MAIN MENU ---------------------");
+        System.out.println("\nPlease Select Option\n");
+        System.out.println("[1] New Account");
+        System.out.println("[2] Balance Inquiry");
+        System.out.println("[3] Deposit");
+        System.out.println("[4] Withdraw");
+        System.out.println("[5] Client Profile");
+        System.out.println("[6] Close Account");
+        System.out.println("[7] Exit");
+        System.out.println("-----------------------------------------------------");
 
     }
 
