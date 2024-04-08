2ef45e4a88808f333bd1dfa26ae9ca0790c461ef
JECLearn <94827754+JECLearn@users.noreply.github.com>
2024-04-08 10:34:10 +0800

    switch case init

--- MachineProblem3/Client.java
+++ MachineProblem3/Client.java
@@ -1,5 +1,7 @@
 package MachineProblem3;
 
+import java.util.Scanner;
+
 /*
 AMAOEd MIS6232 Computer Programming 2 (Java)
 
@@ -12,6 +14,41 @@ Machine Problem 3 (Course Project)
 public class Client {
 
     public static void main(String[] args) {
+
+        SavingsAccount[] sa = new SavingsAccount[100];
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+
+        mainMenu();
+
+        while (true) {
+
+            switch (userInput) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                default:
+                    System.out.println("Wrong input. Try again.");
+                    continue;
+            }
+            break;
+
+        }
+
+    }
+
+    public static void mainMenu() {
 
         // Main Menu Display
         System.out.println("                 Welcome to AMA Bank                 ");
