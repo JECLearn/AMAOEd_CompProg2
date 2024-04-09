7e29c207f8448b44d6416b2d8744e21512ddb159
JECLearn <94827754+JECLearn@users.noreply.github.com>
2024-04-09 10:46:31 +0800

    Lab010 file init

---
+++ Lab010/FileIOExercise.java
@@ -0,0 +1,18 @@
+package Lab010;
+
+/*
+AMAOEd MIS6232 Computer Programming 2 (Java)
+
+Author: J. E. Cunanan
+Student No: 2022-0072254
+
+Laboratory Exercise 010
+*/
+
+import java.io.*;
+
+public class FileIOExercise {
+    public static void main(String[] args) {
+
+    }
+}
