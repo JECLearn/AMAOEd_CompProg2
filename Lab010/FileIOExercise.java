31c80f71f87227583cc1642904bac606605be5ae
JECLearn <94827754+JECLearn@users.noreply.github.com>
2024-04-09 10:55:49 +0800

    deliberate wrong example

--- Lab010/FileIOExercise.java
+++ Lab010/FileIOExercise.java
@@ -13,6 +13,24 @@ import java.io.*;
 
 public class FileIOExercise {
     public static void main(String[] args) {
+        FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
 
+        try {
+            fileInputStream = new FileInputStream("input.txt");
+            fileOutputStream = new FileOutputStream("output.txt");
+
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } finally {
+            if (fileInputStream != null) {
+                fileInputStream.close();
+            }
+            if (fileOutputStream != null) {
+                fileOutputStream.close();
+            }
+        }
     }
 }
