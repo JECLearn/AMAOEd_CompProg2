c8dce26c08e19e4a5dbce55193dc73c566231680
JECLearn <94827754+JECLearn@users.noreply.github.com>
2024-04-11 12:23:31 +0800

    Display, Buttons, Listeners init

--- MachineProblem4/CalcMain.java
+++ MachineProblem4/CalcMain.java
@@ -13,8 +13,37 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class CalcMain {
-    public static void main(String[] args) {
+public class CalcMain extends JFrame implements ActionListener{
+    private JTextField firstNumber;
+    private JTextField secondNumber;
+    private JTextField result;
 
+    public CalcMain() {
+        setTitle("Calculator");
+        setSize(400,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Display
+        firstNumber = new JTextField(10);
+        secondNumber = new JTextField(10);
+        result = new JTextField(10);
+        result.setEditable(false);
+
+        // Buttons
+        JButton addButton = new JButton("ADD");
+        JButton subtractButton = new JButton("SUBTRACT");
+        JButton multiplyButton = new JButton("MULTIPLY");
+        JButton divideButton = new JButton("DIVIDE");
+        JButton resetButton = new JButton("RESET");
+        JButton exitButton = new JButton("EXIT");
+
+        // Button Listeners
+        addButton.addActionListener(this);
+        subtractButton.addActionListener(this);
+        multiplyButton.addActionListener(this);
+        divideButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        exitButton.addActionListener(this);
     }
+
 }
