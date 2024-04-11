50df27faacbfbed5feb256648b4c3c1459aed1ae
JECLearn <94827754+JECLearn@users.noreply.github.com>
2024-04-11 09:17:16 +0800

    SwingExample.java init

---
+++ Lab011/SwingExample.java
@@ -0,0 +1,16 @@
+package Lab011;
+
+/*
+AMAOEd MIS6232 Computer Programming 2 (Java)
+
+Author: J. E. Cunanan
+Student No: 2022-0072254
+
+Laboratory Exercise 011
+*/
+
+public class SwingExample {
+    public static void main(String[] args) {
+
+    }
+}
